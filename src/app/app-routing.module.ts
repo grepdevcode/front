8b80115737e8776f0c28b640bc16d1f09c0dea55
enzomzaocv <enzomzaocv@gmail.com>
2019-12-20 17:47:48 -0300

    auth0 login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   {
@@ -12,7 +13,8 @@ const routes: Routes = [
   },
   {
     path:'cliente',
-    loadChildren:'./cliente/cliente.module#ClienteModule'
+    loadChildren:'./cliente/cliente.module#ClienteModule',
+    canActivate: [AuthGuard]
   }
 ];
 
